ce41419dc775b0ae1539916c2a6add4101014b8e
pavansurange <pavansurange97@gmail.com>
2024-01-22 22:37:45 +0530

    Initial commit with Angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { UserListComponent } from './user-list/user-list.component';
+import { UserUpsertComponent } from './user-upsert/user-upsert.component';
+import { RouterModule, Routes } from '@angular/router';
+const appRoute : Routes = [
+  {path:"userlist", component: UserListComponent},
+  {path:"adduser", component: UserUpsertComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    UserUpsertComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
